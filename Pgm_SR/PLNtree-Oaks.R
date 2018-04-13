d299b613dee0595012ddcbd7004a3396d18a2fa3
“Stéphane <robin@agroparistech.fr>
2018-04-13 13:22:47 +0200

    SR

---
+++ Pgm_SR/PLNtree-Oaks.R
@@ -0,0 +1,54 @@
+rm(list=ls()); par(pch=20); 
+#devtools::install_github("jchiquet/PLNmodels", build_vignettes=TRUE)
+library(PLNmodels); library(sna); 
+source('/home/robin/PgmR/General/FunctionsMatVec.R')
+source('/home/robin/PgmR/Network/FunctionsTree.R')
+source('Functions/FunctionsInference.R')
+library(sna)
+
+# Data
+data.dir = '../Data/Oaks-CVacher/'
+data.name = 'oaks'
+load(paste0(data.dir, data.name, '.RData'))
+
+# Parms
+Y = as.matrix(Data$count); n = nrow(Y); p = ncol(Y)
+O = Data$offset; X = Data$covariates
+
+# PLN models
+PLN.offset = PLN(Y ~ 1 + offset(log(O)))
+PLN.tree = PLN(Y ~ 1 + X$tree + offset(log(O)))
+PLN.tree.distTObase = PLN(Y ~ 1 + X$tree + X$distTObase + offset(log(O)))
+PLN.tree.distTObase.distTOtrunk = PLN(Y ~ 1 + X$tree + X$distTObase + X$distTOtrunk + offset(log(O)))
+
+# BIC
+Crit = rbind(PLN.offset$criteria, PLN.tree$criteria, PLN.tree.distTObase$criteria, PLN.tree.distTObase.distTOtrunk$criteria)
+Crit
+apply(Crit, 2, which.max)
+
+# PLNnetworks
+# PLNnet.tree = PLNnetwork(Y ~ 1 + X$tree + offset(log(O)))
+load('/home/robin/Bureau/CountPCA/sparsepca/Pgm/PLNnetwork/output_oaks/network_oak.RData')
+models_nocov$plot()
+PLNnet_nocov = models_nocov$getBestModel()
+G_nocov = 1*(PLNnet_nocov$model_par$Omega!=0)
+Pos = gplot(G_nocov, gmode='graph', label=colnames(Y))
+
+models$plot()
+PLNnet_cov = models$getBestModel()
+G_cov = 1*(PLNnet_cov$model_par$Omega!=0)
+gplot(G_cov, gmode='graph', label=colnames(Y), coord=Pos)
+
+plot(colSums(G_nocov), colSums(G_cov))
+
+# # EM beta
+# beta.unif = matrix(1, p, p); diag(beta.unif) = 0; beta.unif = beta.unif / sum(beta.unif)
+# P.unif = Kirshner(beta.unif)$P
+# alpha = 1
+# Cor = cov2cor(PLN.tree$model_par$Sigma)
+# log.phi = -.5*n*alpha*log(1-Cor^2); 
+# log.phi = log.phi - mean(log.phi[upper.tri(log.phi)]); diag(log.phi) = 0
+# phi = exp(log.phi); diag(phi) = 0
+# PedgeY.1step = KirshnerRM(beta.unif*phi)$P; sum(PedgeY.1step)
+# FitEM = FitBetaStatic(beta.init=beta.unif, phi=phi)
+# image(FitEM$beta)
