92bc7c26a591f1f19ea706607e17cc45c5a68622
Stéphane ROBIN <robin@agroparistech.fr>
2020-06-15 12:55:45 +0200

    tree sampling

--- Pgm_SR/TreeSampling.R
+++ Pgm_SR/TreeSampling.R
@@ -7,66 +7,74 @@ source('Functions/FunctionsTree.R')
 par(mfcol=c(3, 2), mex=.6, pch=20)
 
 # Dims
-p <- 15; P <- p*(p-1)/2; B <- 1e3
+p <- 10; P <- p*(p-1)/2; B <- 1e3
 seed <- 1; set.seed(seed)
 
 # Parms
 unifCst <- SumTree(matrix(1, p, p))
 beta <- F_Vec2Sym(F_Sym2Vec(matrix(rgamma(p^2, 1, 2), p, p)))
-beta <- beta / SumTree(beta)^(1/(p-1))
-betaVec <- F_Sym2Vec(beta)
-prob <- EdgeProba(beta); diag(prob) <- 0
-probVec <- F_Sym2Vec(prob)
+beta <- beta / SumTree(beta)^(1/(p-1)); betaVec <- F_Sym2Vec(beta)
+
+# Edge prob
+prob <- EdgeProba(beta); diag(prob) <- 0; probVec <- F_Sym2Vec(prob)
 print(c(2/p, mean(probVec)))
-hist(betaVec, breaks=p)
-hist(probVec, breaks=p)
+hist(betaVec, breaks=p); hist(probVec, breaks=p)
 plot(betaVec, probVec); abline(0, 1); abline(0, sqrt(p)); abline(0, max(prob/beta, na.rm=TRUE))
 
 # Function
-rSpanTreeV0 <- function(prob){
-   # Approximate sampling of a spanning according to edge probabilities
-   # Frequency of edge selection empricially OK
-   # No guaranty about the actual distribution if the whole tree
-   p <- nrow(prob)
-   graph <- F_Vec2Sym(F_Sym2Vec(matrix(rbinom(p^2, 1, prob), p, p)))
-   while(!is.connected(graph)){graph <- F_Vec2Sym(F_Sym2Vec(matrix(rbinom(p^2, 1, prob), p, p)))}
-   w <- F_Vec2Sym(F_Sym2Vec(matrix(runif(p^2), p, p)))
-   w <- -log(w)
-   w[which(graph==0)] <- Inf
-   return(mst(w))
-}
+# rSpanTreeV0 <- function(prob){
+#    # Approximate sampling of a spanning according to edge probabilities
+#    # Frequency of edge selection empricially OK
+#    # No guaranty about the actual distribution if the whole tree
+#    p <- nrow(prob)
+#    graph <- F_Vec2Sym(F_Sym2Vec(matrix(rbinom(p^2, 1, prob), p, p)))
+#    while(!is.connected(graph)){graph <- F_Vec2Sym(F_Sym2Vec(matrix(rbinom(p^2, 1, prob), p, p)))}
+#    w <- F_Vec2Sym(F_Sym2Vec(matrix(runif(p^2), p, p)))
+#    w <- -log(w)
+#    w[which(graph==0)] <- Inf
+#    return(mst(w))
+# }
 
-# Function
 rSpanTreeV1 <- function(beta, prob){
    # Approximate sampling of a spanning according to edge probabilities
    # Rejection sampling approach
    # !!! Beta's must be normalized !!! (constant B = 1)
    # Max ratio between proposal and target heuristicaly set to p^2
-   p <- nrow(prob); 
+   p <- nrow(prob); P <- p*(p-1)/2
    prob <- prob / max(prob) # To enforce connectivity
    probVec <- F_Sym2Vec(prob); betaVec <- F_Sym2Vec(beta)
-   maxRatioVec <- max(probVec / betaVec); M <- maxRatioVec^(p-1)
    M <- p^2
+   # m <- min(probVec / betaVec); M <- p^(p-2) / m^(p-1)
    OK <- FALSE; tries <- 0; pTree <- qTree <- rep(0, 1e4)
    while(!OK){
       tries <- tries + 1
       
-      graph <- F_Vec2Sym(matrix(rbinom(length(probVec), 1, probVec)))
-      while(!is.connected(graph)){graph <- F_Vec2Sym(matrix(rbinom(length(probVec), 1, probVec)))}
+      # graph = heterogeneous ER connected graph
+      graph <- F_Vec2Sym(matrix(rbinom(P, 1, probVec)))
+      while(!is.connected(graph)){graph <- F_Vec2Sym(matrix(rbinom(P, 1, probVec)))}
       # gplot(graph, gmode='graph', main=round(SumTree(graph)))
       graphVec <- F_Sym2Vec(graph)
-      qGraph <- prod(dbinom(graphVec, 1, probVec))
+      # qGraph <- prod(dbinom(graphVec, 1, probVec))
       
+      # tree = uniform spanning tree
       w <- F_Vec2Sym(F_Sym2Vec(matrix(runif(p^2), p, p)))
-      w <- -log(w)
       w[which(graph==0)] <- Inf
       tree <- mst(w)
       treeVec <- F_Sym2Vec(tree)
 
+      # Monte-Carlo sample of graphs containing the tree
+      biasedProbVec <- probVec; biasedProbVec[which(treeVec==1)] <- 1
+      invSpanTreeMean <- 0; G <- 1e3
+      for(g in 1:G){invSpanTreeMean <- invSpanTreeMean + 1/SumTree(F_Vec2Sym(matrix(rbinom(P, 1, biasedProbVec))))}
+      invSpanTreeMean <- invSpanTreeMean/G
+      qTree[tries] <- prod(probVec[which(treeVec==1)]) * invSpanTreeMean
+      
       # qTree[tries] <- qGraph / SumTree(graph)
-      qTree[tries] <- prod(probVec[which(treeVec==1)]) / SumTree(graph)
+      # Actually qTree should be qGraph / SumTree(graph)
+      # qTree[tries] <- prod(probVec[which(treeVec==1)]) / SumTree(graph)
       pTree[tries] <- prod(betaVec[which(treeVec==1)])
 
+      if(pTree[tries] > (M * qTree[tries])){cat('[*] ')}
       if(runif(1) < pTree[tries] / (M * qTree[tries])){OK <- TRUE}
    }
    pTree <- pTree[1:tries]; qTree <- qTree[1:tries]
@@ -92,7 +100,7 @@ for(b in 1:B){
 sum(edgeFreq)
 edgeFreq <- edgeFreq/B
 print(c(2/p, mean(probVec), mean(edgeFreq)))
-rbind(summary(tries), summary(minRatio), summary(maxRatio))
+print(rbind(summary(tries), summary(minRatio), summary(maxRatio)))
 
 
 # Plots
