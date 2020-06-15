846d35ee30448bebf3e9d272eab0cc0f87759dbd
Stéphane ROBIN <robin@agroparistech.fr>
2020-06-15 08:16:41 +0200

    tree sampling

---
+++ Pgm_SR/TreeSampling.R
@@ -0,0 +1,46 @@
+# Sampling from the tree distribution
+
+rm(list=ls()); par(pch=20)
+library(sna); library(ape)
+source('Functions/FunctionsMatVec.R')
+source('Functions/FunctionsTree.R')
+
+# Dims
+p <- 10; P <- p*(p-1)/2
+B <- 1e4
+
+# Parms
+beta <- F_Vec2Sym(F_Sym2Vec(matrix(exp(runif(p^2)), p, p)))
+prob <- EdgeProba(beta); diag(prob) <- 0
+probVec <- F_Sym2Vec(prob)
+print(c(2/p, mean(probVec)))
+
+# Function
+rSpanTree <- function(prob){
+   p <- nrow(prob)
+   g <- F_Vec2Sym(F_Sym2Vec(matrix(rbinom(p^2, 1, prob), p, p)))
+   while(!is.connected(g)){g <- F_Vec2Sym(F_Sym2Vec(matrix(rbinom(p^2, 1, prob), p, p)))}
+   w <- F_Vec2Sym(F_Sym2Vec(matrix(runif(p^2), p, p)))
+   w <- -log(w)
+   w[which(g==0)] <- Inf
+   return(mst(w))
+}
+
+# Tree sampling
+edgeFreq <- rep(0, P)
+for(b in 1:B){
+   if(b%%round(sqrt(B))==0){cat(b, '')}
+   tree <- rSpanTree(prob)
+   edgeFreq <- edgeFreq + F_Sym2Vec(tree)
+}
+sum(edgeFreq)
+edgeFreq <- edgeFreq/B
+print(c(2/p, mean(probVec), mean(edgeFreq)))
+
+# Plots
+par(mfrow=c(2, 1))
+plot(sort(edgeFreq))
+plot(probVec, edgeFreq-probVec); 
+abline(0, 0)
+lines(sort(probVec), qbinom(.025, B, sort(probVec))/B-sort(probVec), lty=2)
+lines(sort(probVec), qbinom(.975, B, sort(probVec))/B-sort(probVec), lty=2)
